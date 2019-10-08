8de596f23034761f4321989ffde3681f91c421ed
Zeromile <jasonleecooksey@gmail.com>
2019-10-08 16:01:45 -0700

    login changes??

--- login.php
+++ login.php
@@ -14,7 +14,7 @@ if (!empty($_POST)){
   if ($userName == $row["username"] &&
     md5($passWord) == $row["password"])
     {
-      $_SESSION["`loggedin`"] = "logged in";
+      $_SESSION["loggedin"] = "logged in";
       $_SESSION["realname"] = $row["realname"];
       $loggedIn = $_SESSION["loggedin"];
     } else {
