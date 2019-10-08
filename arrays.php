8d4add55802b06ebde28f3453dd97c8d83c1c3fa
Zeromile <jasonleecooksey@gmail.com>
2019-10-08 15:12:03 -0700

    start of day 12

---
+++ arrays.php
@@ -0,0 +1,12 @@
+<?php
+$myIndexArray = array("Katie", "Em", "Molly");
+
+$myAssocArray = array(
+  "name" => "Katie",
+  "latinx" => True,
+  "cohort" => "design"
+);
+
+$myOtherAssocArray = array();
+
+$myOtherAssocArray["name"] = "Nikki";
